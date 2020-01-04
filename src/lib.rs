bfd7fff0840749912146b4db70377b70109790ae
Robin Lindner <robin@deeprobin.de>
2020-01-05 00:27:45 +0100

    :art: Improved code style

--- src/lib.rs
+++ src/lib.rs
@@ -7,46 +7,63 @@ extern crate no_std_compat as std;
 use std::prelude::v1::*;
 
 #[cfg(test)]
+
 mod tests {
 
     #[cfg(feature = "constants")]
     #[test]
+
     fn test_cracker() {
+
         let mut x = crate::BruteForce::new(crate::UPPERCASE_CHARS);
+
         let password = "PASS";
+
         let mut trys: i32 = 0;
+
         loop {
+
             trys = trys + 1;
+
             let out = x.raw_next();
+
             if out == password.to_string() {
+
                 println!(">>> SUCCESS ({} times)", trys);
+
+                break;
             }
         }
     }
 }
 
 #[cfg(feature = "constants")]
+
 pub const UPPERCASE_CHARS: &'static [char] = &[
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
 #[cfg(feature = "constants")]
+
 pub const LOWERCASE_CHARS: &'static [char] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
 #[cfg(feature = "constants")]
+
 pub const NUMBER_CHARS: &'static [char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 #[cfg(feature = "constants")]
+
 pub const SPECIAL_CHARS: &'static [char] = &[
     '!', '\"', '\'', '?', '\\', '#', '$', '§', '%', '&', '/', '(', ')', '=', '[', ']', '(', ')',
     '{', '}', '´', '`', '<', '>', '€', ',', '.', '-', '_',
 ];
 
 /// Represents a brute-forcing instance
+
 pub struct BruteForce {
     /// Represents the charset of the brute-forcer
     pub chars: &'static [char],
@@ -76,7 +93,9 @@ impl BruteForce {
     ///    }
     /// }
     /// ```
+
     pub fn new(charset: &'static [char]) -> BruteForce {
+
         BruteForce {
             chars: charset,
             current: charset[0].to_string(),
@@ -105,11 +124,16 @@ impl BruteForce {
     ///    }
     /// }
     /// ```
+
     pub fn new_at(charset: &'static [char], start: usize) -> BruteForce {
+
         let mut start_string = String::new();
+
         for _ in 0..start {
+
             start_string.push(charset[0]);
         }
+
         BruteForce {
             chars: charset,
             current: start_string,
@@ -138,7 +162,9 @@ impl BruteForce {
     ///    }
     /// }
     /// ```
+
     pub const fn new_by_start_string(charset: &'static [char], start_string: String) -> BruteForce {
+
         BruteForce {
             chars: charset,
             current: start_string,
@@ -146,74 +172,107 @@ impl BruteForce {
     }
 
     /// This returns the next element without unnecessary boxing in a Option
+
     pub fn raw_next(&mut self) -> String {
+
         let current_chars: Vec<char> = self.current.chars().collect();
+
         let mut s: String = String::new();
+
         let len: usize = *&current_chars.len();
+
         for n in 0..len {
+
             let c = &current_chars[n];
+
             if n != (len - 1) {
+
                 if self.are_next_chars_last(&current_chars, n + 1) {
+
                     s.push(*self.next_char(c));
                 } else {
+
                     s.push(*c);
                 }
             } else if self.is_last_char(c) {
+
                 if self.are_all_chars_last(&current_chars) {
+
                     s.push(*self.first_char());
+
                     s.push(*self.first_char());
                 } else {
+
                     s.push(*self.first_char());
                 }
             } else {
+
                 s.push(*self.next_char(c));
             }
         }
+
         self.current = s.clone();
+
         return s;
     }
 
     fn are_next_chars_last(&self, chars: &Vec<char>, start: usize) -> bool {
+
         for n in start..chars.len() {
+
             if !self.is_last_char(&chars[n]) {
+
                 return false;
             }
         }
+
         return true;
     }
 
     fn are_all_chars_last(&self, chars: &Vec<char>) -> bool {
+
         self.are_next_chars_last(chars, 0)
     }
 
     fn next_char(&self, c: &char) -> &char {
+
         let mut is_next: bool = false;
+
         for ch in self.chars {
+
             if is_next {
+
                 return ch;
             } else if ch == c {
+
                 is_next = true;
             }
         }
+
         return &self.chars[0];
     }
 
     fn is_last_char(&self, c: &char) -> bool {
+
         self.last_char() == c
     }
 
     const fn first_char(&self) -> &char {
+
         &self.chars[0]
     }
 
     const fn last_char(&self) -> &char {
+
         &self.chars[self.chars.len() - 1]
     }
 }
 
 impl Iterator for BruteForce {
     type Item = String;
+
     fn next(&mut self) -> Option<String> {
+
         Some(self.raw_next())
     }
 }
